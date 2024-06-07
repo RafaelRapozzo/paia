c7de2cbf9d8408460ea0f781baee3d1259fa6de3
Rafael Raposo <rrafa7124@gmail.com>
2024-06-06 22:42:31 -0300

    amor

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <script src="https://kit.fontawesome.com/6dda5f6271.js" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="slide.css">
+    <script src="slide.js" defer></script>
+    <link rel="icon" type="image/png" href="/img/favicon.png">
+    <title>Presente</title>
+    
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="menu">
+                <nav>
+                    <a href="#historia">História</a>
+                    <a href="#motivos">Motivos</a>
+                    <a href="#equipe">nossa equipe</a>
+                    <a href="#servicos">serviços</a>
+                    <a href="#contato">contato</a>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <section class="slider">
+        <div class="slider-content">
+            
+            <input type="radio" name="btn-radio" id="radio1">
+            <input type="radio" name="btn-radio" id="radio2">
+            <input type="radio" name="btn-radio" id="radio3">
+
+            <div class="slide-box primeiro">
+                <img class="img-desktop" src="img/slide1.png" >
+                <img class="img-mobile" src="img/slide1.png" >
+            </div>
+
+            <div class="slide-box">
+                <img class="img-desktop" src="img/slide2.png" >
+                <img class="img-mobile" src="img/slide2.png" >
+            </div>
+
+            <div class="slide-box">
+                <img class="img-desktop" src="img/slide3.png" >
+                <img class="img-mobile" src="img/slide3.png" >
+            </div>
+
+           
+            
+
+        </div>
+    </section>
+ 
+    <section id="historia">
+        <div class="container1">
+            <h2>Amor te Amo</h2>
+            <p>A nossa história começa numa escola chamada matteo bei, lá passamos muitos bons momentos, te amei em segredo igual você me amava também, sem saber nós criamos uma conexão incrivel e que dura até hoje, o nosso amor ainda incendeia e arde até hoje. fiz esse site simplezinho só pra dizer que te amo hhihihiihih</p>
+        </div>
+    
+</body>
+</html>
